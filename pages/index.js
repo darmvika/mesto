2720abe5d2187e4716844a17dfe7355ccc76a852
Viktoriya Darmina <vikadarmina5@gmail.com>
2023-04-08 01:53:41 +0300

    Ревью

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,39 @@
-const openPopupBtn = document.querySelector('.profile__info-rectangle__rectangle');
-const popup = document.querySelector('.popup');
-const popupCloseBtn = document.querySelector('.popup__content__button');
+const openPopupBtn = document.querySelector ('.profile__button-redact');
+const popup = document.querySelector ('.popup');
+const btnClose = document.querySelector ('.content__button-close');
+const btnSave = document.querySelector ('.content__button-save');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
+const nameInput = document.querySelector('.content__input_name');
+const jobInput = document.querySelector('.content__input_job');
+const formElement = document.querySelector('.popup__content');
 
-function toggleOpup() {
-    popup.classList.toggle('popup_open');
+function openPopup(){
+    popup.classList.add('popup_opened');
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
+
+
+}
+openPopupBtn.addEventListener('click', openPopup);
+
+function closePopup (){
+    popup.classList.remove('popup_opened');
+}
+btnClose.addEventListener('click', closePopup);
+
+nameInput.value = profileName.textContent;
+jobInput.value = profileJob.textContent;
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    nameInput = document.querySelector('.content__input_name');
+    jobInput = document.querySelector('.content__input_job');
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
 }
 
-openPopupBtn.addEventListener('click', toggleOpup);
 
-popupCloseBtn.addEventListener('click', toggleOpup);
+formElement.addEventListener('submit', handleFormSubmit); 
